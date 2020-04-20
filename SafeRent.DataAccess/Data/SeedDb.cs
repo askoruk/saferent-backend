d243eae384cca00078555d2eec6c5942ef59f5d9
Oleksandr Skoruk <oleksandr.skoruk@nure.ua>
2020-04-20 14:24:09 +0300

    setup aspnetcore identity

---
+++ SafeRent.DataAccess/Data/SeedDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+namespace SafeRent.DataAccess.Data
+{
+    public class SeedDb
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<AppDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+            if (context.Users.Any()) return;
+            var user = new ApplicationUser()
+            {
+                Email = "ali@gmail.com",
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = "Ali"
+            };
+            userManager.CreateAsync(user, "Ali@123");
+        }
+    }
+}
--- SafeRent/Startup.cs
+++ SafeRent/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
+using SafeRent.DataAccess.Data;
 
 namespace SafeRent
 {
@@ -23,19 +18,27 @@ namespace SafeRent
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddMvc();
+            
+            services.AddDbContext<AppDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            SeedDb.Initialize(app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope().ServiceProvider);
 
             app.UseHttpsRedirection();
 
