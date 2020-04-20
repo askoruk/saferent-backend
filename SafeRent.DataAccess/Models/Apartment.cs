94eb62ce72def57332da4f2564499e012baa3b30
Oleksandr Skoruk <oleksandr.skoruk@nure.ua>
2020-04-20 20:32:33 +0300

    add models

---
+++ SafeRent.DataAccess/Models/Apartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SafeRent.DataAccess.Models
+{
+    public class Apartment
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Address { get; set; }
+        public bool AnimalsAllowed { get; set; }
+        public int RentalDays { get; set; }
+        public bool AvailableForRent { get; set; }
+    }
+}
---
+++ SafeRent.DataAccess/Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SafeRent.DataAccess.Models
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Message { get; set; }
+        public string DateSend { get; set; }
+    }
+}
